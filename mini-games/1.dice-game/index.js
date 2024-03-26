b3debfc454967a93e505d399f6790de0cd478b46
StKrz <stavroskyriazopoulos@gmail.com>
2024-03-26 06:18:48 +0200

    mini games

---
+++ mini-games/1.dice-game/index.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('button').addEventListener('click', function() {
+        let randomNumber1 = Math.floor(Math.random() * 6) + 1;
+        let randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+        document.querySelector('.img1').setAttribute('src', `./images/dice${randomNumber1}.png`);
+        document.querySelector('.img2').setAttribute('src', `./images/dice${randomNumber2}.png`);
+
+        if (randomNumber1 === randomNumber2) {
+            document.querySelector('h1').innerHTML = '🚩Draw🚩';
+        } else if (randomNumber1 > randomNumber2) {
+            document.querySelector('h1').innerHTML = '🚩Player 1 Wins!';
+        } else if (randomNumber1 < randomNumber2) {
+            document.querySelector('h1').innerHTML = '🚩Player 2 Wins!';
+        }
+    });
+});
